0474428d5ae79450824375cf9c423a4157fa5f00
Linda Barbiero <linda.barbiero.st@everis.com>
2019-10-08 14:25:00 +0100

    updated files

--- arrays.js
+++ arrays.js
@@ -2,7 +2,11 @@
 
 let v = [1,4,5,2,0];
 
-const different = [1, "fruit", true, 5.22, "yes"];
+let different = [1, "fruit", true, 5.22, "yes"];
 console.log(v[1]);
+
+different[2]=false;
+different.push("trying");
+different.unshift("strawberries");
 
 console.log(different);
