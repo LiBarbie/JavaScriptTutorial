a2029ea37713dba03814fdff55911c20fcf9cd17
Linda Barbiero <linda.barbiero.st@everis.com>
2019-10-08 14:20:06 +0100

    changed b.js

--- b.js
+++ b.js
@@ -6,5 +6,11 @@ console.log('My name is '+name+'\nand my age is '+age);
 const s ="Hello World!";
 
 //Substrings
-console.log(s.substr(0,5));
+console.log(s.substr(0,5).toUpperCase());
 console.log(s.substring(0,5));
+
+console.log(s.split(''));
+
+const q = "technology, it, science, orange, pizza";
+
+console.log(q.split(', '));
